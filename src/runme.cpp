a8df6d6f1bb65f8b63877c14bb123fc46847d5d1
ros07 <ros07@example.com>
2018-01-09 16:01:39 -0500

    first commit

---
+++ src/runme.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ros/ros.h>
+#include "triclops_vision/vision_3d.h"
+#include "triclops_vision/line_filter.h"
+#include "triclops_vision/camera_system.h"
+
+int main (int argc, char **argv) {
+	ros::init(argc,argv,"triclops_vision");
+	ros::NodeHandle nh;
+  	ros::Rate loop_rate(10);
+
+	CameraSystem camera(argc,argv);
+	LineFilter linefilter(argc,argv);
+    Vision3D vision3D(argc, argv, &camera);
+
+	while (ros::ok()) {
+		camera.run();
+		linefilter.run();
+        vision3D.run();
+        linefilter.displayHough();
+		loop_rate.sleep();
+	}	
+}
